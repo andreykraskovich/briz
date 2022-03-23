5f0d39628e07d75a0b4a468929b4cb5577f48c30
Andrew <98891710+andreykraskovich@users.noreply.github.com>
2022-03-23 16:29:20 +0300

    e

--- bizz/target/bizz/WEB-INF/classes/db/migration/V2__Create_Form.sql
+++ bizz/target/bizz/WEB-INF/classes/db/migration/V2__Create_Form.sql
@@ -1,1 +1,1 @@
-CREATE TABLE Forms(id INT, year INT, code VARCHAR(10), teacher_id INT, students_id integer[]);
+CREATE TABLE Forms(id INT, year INT, code VARCHAR(10), teachers_id INT, students_id integer[]);
--- bizz/target/bizz/WEB-INF/classes/db/migration/V3__Insert.sql
+++ bizz/target/bizz/WEB-INF/classes/db/migration/V3__Insert.sql
@@ -1,16 +1,16 @@
-INSERT INTO Students(id, surname, name, lastname, birthday, gender) values (0, 'petrov', 'petr', 'petrovich', 2007, 'm');
-INSERT INTO Students(id, surname, name, lastname, birthday, gender) values (1, 'petrov', 'petr', 'petrovich', 2007, 'm');
-INSERT INTO Students(id, surname, name, lastname, birthday, gender) values (2, 'petrov', 'petr', 'petrovich', 2007, 'm');
-INSERT INTO Students(id, surname, name, lastname, birthday, gender) values (3, 'petrov', 'petr', 'petrovich', 2007, 'm');
-INSERT INTO Students(id, surname, name, lastname, birthday, gender) values (5, 'petrov', 'petr', 'petrovich', 2007, 'm');
-INSERT INTO Students(id, surname, name, lastname, birthday, gender) values (6, 'petrov', 'petr', 'petrovich', 2007, 'm');
+INSERT INTO Students(students_id, surname, name, lastname, birthday, gender) values (0, 'petrov', 'petr', 'petrovich', 2007, 'm');
+INSERT INTO Students(students_id, surname, name, lastname, birthday, gender) values (1, 'peov', 'petr', 'petrovich', 2007, 'm');
+INSERT INTO Students(students_id, surname, name, lastname, birthday, gender) values (2, 'ptrov', 'petr', 'petrovich', 2007, 'm');
+INSERT INTO Students(students_id, surname, name, lastname, birthday, gender) values (3, 'etrov', 'petr', 'petrovich', 2007, 'm');
+INSERT INTO Students(students_id, surname, name, lastname, birthday, gender) values (5, 'petro', 'petr', 'petrovich', 2007, 'm');
+INSERT INTO Students(students_id, surname, name, lastname, birthday, gender) values (6, 'perov', 'petr', 'petrovich', 2007, 'm');
 
-INSERT INTO Teachers(id, surname, name, lastname, birthday, gender, object) values (0, 'ivanov', 'ivan', 'ivanovich', 1997, 'm', 'history');
-INSERT INTO Teachers(id, surname, name, lastname, birthday, gender, object) values (1, 'ivanov', 'ivan', 'ivanovich', 1997, 'm', 'history');
-INSERT INTO Teachers(id, surname, name, lastname, birthday, gender, object) values (2, 'ivanov', 'ivan', 'ivanovich', 1997, 'm', 'history');
-INSERT INTO Teachers(id, surname, name, lastname, birthday, gender, object) values (3, 'ivanov', 'ivan', 'ivanovich', 1997, 'm', 'history');
-INSERT INTO Teachers(id, surname, name, lastname, birthday, gender, object) values (4, 'ivanov', 'ivan', 'ivanovich', 1997, 'm', 'history');
-INSERT INTO Teachers(id, surname, name, lastname, birthday, gender, object) values (5, 'ivanov', 'ivan', 'ivanovich', 1997, 'm', 'history');
+INSERT INTO Teachers(teachers_id, surname, name, lastname, birthday, gender, object) values (0, 'ivanov', 'ivan', 'ivanovich', 1997, 'm', 'history');
+INSERT INTO Teachers(teachers_id, surname, name, lastname, birthday, gender, object) values (1, 'ivanov', 'ivan', 'ivanovich', 1997, 'm', 'history');
+INSERT INTO Teachers(teachers_id, surname, name, lastname, birthday, gender, object) values (2, 'ivanov', 'ivan', 'ivanovich', 1997, 'm', 'history');
+INSERT INTO Teachers(teachers_id, surname, name, lastname, birthday, gender, object) values (3, 'ivanov', 'ivan', 'ivanovich', 1997, 'm', 'history');
+INSERT INTO Teachers(teachers_id, surname, name, lastname, birthday, gender, object) values (4, 'ivanov', 'ivan', 'ivanovich', 1997, 'm', 'history');
+INSERT INTO Teachers(teachers_id, surname, name, lastname, birthday, gender, object) values (5, 'ivanov', 'ivan', 'ivanovich', 1997, 'm', 'history');
 
-INSERT INTO Forms(id, year, code, teacher_id, students_id) values (0, 2012, 'a', 0, ARRAY [0,1,2,3,4]);
+INSERT INTO Forms(id, year, code, teachers_id, students_id) values (0, 2012, 'a', 0, ARRAY [0,1,2,3,4]);
 
